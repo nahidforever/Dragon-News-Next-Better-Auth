4f16127a56a461a2da804da1f60d9b698b436b18
Nahid1828 <isam15-5334@diu.edu.bd>
2026-04-27 20:35:10 +0600

    Social Sign-In (Google And Github)

--- src/components/homepage/news/RightSidebar.jsx
+++ src/components/homepage/news/RightSidebar.jsx
@@ -1,16 +1,37 @@
+"use client";
+
+import { authClient } from "@/lib/auth-client";
 import React from "react";
 import { FaGithub, FaGoogle } from "react-icons/fa";
 
 const RightSidebar = () => {
+  const handleGoogleSignIn = async () => {
+    const data = await authClient.signIn.social({
+      provider: "google",
+    });
+
+    console.log(data, "Data");
+  };
+
+  const handleGithubSignIn = async () => {
+    const data = await authClient.signIn.social({
+      provider: "github",
+    });
+    console.log(data, "Data");
+  };
+
   return (
     <div>
       <h2 className="font-bold text-lg mb-4">Login with</h2>
       <div className="flex flex-col gap-2">
-        <button className="btn border-blue-500 text-blue-500">
+        <button
+          className="btn border-blue-500 text-blue-500"
+          onClick={handleGoogleSignIn}
+        >
           <FaGoogle />
           Login with google
         </button>
-        <button className="btn">
+        <button className="btn" onClick={handleGithubSignIn}>
           <FaGithub />
           Login with github
         </button>
